59360dbf9b2a08943eba4e03f32991696a7d82e0
Kankan Deka <kdekamail@gmail.com>
2019-10-03 11:35:22 +0530

    Updates

---
+++ precache-manifest.aaccc44ac1b2cb488cabd089ddd7f81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9228e2b9280103721405b1bb331809a0",
+    "url": "/React_Material_UI/index.html"
+  },
+  {
+    "revision": "a7985b05c4ccdd6a6fde",
+    "url": "/React_Material_UI/static/css/2.04b26184.chunk.css"
+  },
+  {
+    "revision": "063c4b3d5b3f7d4489be",
+    "url": "/React_Material_UI/static/css/main.cde9bc90.chunk.css"
+  },
+  {
+    "revision": "a7985b05c4ccdd6a6fde",
+    "url": "/React_Material_UI/static/js/2.72f6665b.chunk.js"
+  },
+  {
+    "revision": "063c4b3d5b3f7d4489be",
+    "url": "/React_Material_UI/static/js/main.027e22c2.chunk.js"
+  },
+  {
+    "revision": "0b83aea70d9a4104ce87",
+    "url": "/React_Material_UI/static/js/runtime~main.7a34df71.js"
+  }
+]);
